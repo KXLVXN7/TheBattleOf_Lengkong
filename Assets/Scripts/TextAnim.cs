28522f2920fe085d3df3b4a6ae53c6972489cbeb
Kelvin <kelvinshinx@gmail.com>
2023-12-06 15:29:06 +0700

    Update TextAnim.cs

--- Assets/Scripts/TextAnim.cs
+++ Assets/Scripts/TextAnim.cs
@@ -3,73 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-namespace SheviaFireX
+public class TextAnim : MonoBehaviour
 {
-    public class TextAnim : MonoBehaviour
+    //public AnimatorHandler animatorHandler;
+    public TextMeshProUGUI _textMeshPro;
+
+    public string stringArray;
+
+    [SerializeField] float timeBtwnChars;
+    //[SerializeField] float timeBtwnWords;
+
+    int i = 0;
+
+    void OnEnable()
     {
-        //public AnimatorHandler animatorHandler;
-        public TextMeshProUGUI _textMeshPro;
+        _textMeshPro = GetComponent<TextMeshProUGUI>();
+        stringArray = _textMeshPro.text;
+        EndCheck();
+    }
 
-        public string stringArray;
-
-        [SerializeField] float timeBtwnChars;
-        //[SerializeField] float timeBtwnWords;
-
-        int i = 0;
-
-        void OnEnable()
+    public void EndCheck()
+    {
+        if (i <= stringArray.Length - 1)
         {
-            _textMeshPro = GetComponent<TextMeshProUGUI>();
-            stringArray = _textMeshPro.text;
-            EndCheck();
-
-
+            //animatorHandler.TriggerGoTalk();
+            _textMeshPro.text = stringArray;
+            StartCoroutine(TextVisible());
         }
+    }
 
-        public void EndCheck()
+    public IEnumerator TextVisible()
+    {
+        _textMeshPro.ForceMeshUpdate();
+        int totalVisibleCharacters = _textMeshPro.textInfo.characterCount;
+        int counter = 0;
+        while (true)
         {
-            if (i <= stringArray.Length - 1)
+            int visibleCount = counter % (totalVisibleCharacters + 1);
+            _textMeshPro.maxVisibleCharacters = visibleCount;
+
+            if (visibleCount >= totalVisibleCharacters)
             {
-                //animatorHandler.TriggerGoTalk();
-                _textMeshPro.text = stringArray;
-                StartCoroutine(TextVisible());
-            }
-
-
-        }
-
-        public IEnumerator TextVisible()
-        {
-
-            _textMeshPro.ForceMeshUpdate();
-            int totalVisibleCharacters = _textMeshPro.textInfo.characterCount;
-            int counter = 0;
-            while (true)
-            {
-                int visibleCount = counter % (totalVisibleCharacters + 1);
-                _textMeshPro.maxVisibleCharacters = visibleCount;
-
-                if (visibleCount >= totalVisibleCharacters)
+                i += 1;
+                if (i > stringArray.Length - 1) // cek apakah sudah sampai ke teks terakhir
                 {
-                    i += 1;
-                    if (i > stringArray.Length - 1) // cek apakah sudah sampai ke teks terakhir
-                    {
-                        yield break; // hentikan coroutine
-                    }
-                    else
-                    {
-                        //Invoke("EndCheck", 0);
-                        //Debug.Log("end");    
-                        break;
-                    }
+                    yield break; // hentikan coroutine
                 }
-
-                counter += 1;
-                yield return new WaitForSeconds(timeBtwnChars);
+                else
+                {
+                    break;
+                }
             }
 
-            //animatorHandler.TriggerGoIdle();
-
+            counter += 1;
+            yield return new WaitForSeconds(timeBtwnChars);
         }
+
+        //animatorHandler.TriggerGoIdle();
     }
 }
